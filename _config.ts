54f129546faa5944bb0686e2b502468fa0fa473f
Ohkubo KOHEI <kuboon@trick-with.net>
2022-04-28 01:50:02 +0000

    add middleware

--- _config.ts
+++ _config.ts
@@ -4,11 +4,24 @@ import bundler from "lume/plugins/bundler.ts";
 import code_highlight from "lume/plugins/code_highlight.ts";
 import inline from "lume/plugins/inline.ts";
 import jsx from "lume/plugins/jsx.ts";
+import { Middleware } from "lume/core.ts";
+
+const github_pages: Middleware = async (req, next) => {
+  const response = await next(req);
+  if(response.status !== 404) {
+    return response;
+  }
+  const req2 = new Request(req.url + ".html", req);
+  return next(req2);
+}
 
 const site = lume({
   src: './pages',
   dest: './docs',
   prettyUrls: false,
+  server: {
+    middlewares: [github_pages],
+  }
 });
 site.data('layout', 'layout.njk');
 
